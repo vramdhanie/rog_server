001ed5872b352c2b7d14f1e90303170056bbf51a
Vincent Ramdhanie <vramdhanie@gmail.com>
2018-02-25 10:53:05 -0400

    Add JWT constants to config

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 'use strict';
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/rog';
 exports.PORT = process.env.PORT || 8080;
+exports.JWT_SECRET = process.env.JWT_SECRET || 'pulmonary';
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
