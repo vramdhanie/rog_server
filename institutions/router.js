d36321b4068bd8cb19cfd61a4970303f68ab9299
Vincent Ramdhanie <vramdhanie@gmail.com>
2018-03-01 10:57:02 -0400

    Document the get router for institution

--- institutions/router.js
+++ institutions/router.js
@@ -3,6 +3,23 @@ const express = require('express');
 const router = express.router();
 const Institution = require('./model');
 
+/**
+ * @api {get} /institution/ Request all institutions
+ * @apiName getInstitutions
+ * @apiGroup Institution
+ *
+ * @apiSuccess (200){Object[]} institutions List of institutions
+ * @apiSuccessExample {json} Success-response:
+ *
+ *      HTTP/1.1 200 OK
+ *      [
+ *        {
+ *          "name": String,
+ *          "documents: [Document]
+ *        }
+ *      ]
+ *
+ */
 router.get('/', (req, res) => {
   Institution
       .find()
